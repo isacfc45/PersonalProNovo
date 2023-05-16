886cc1e96edd63d472902d78878bad425a2858ce
isacfc45 <isacferreira2007@hotmail.com>
2023-05-16 19:06:12 -0300

    reinicio

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Routes from './app/routes/Routes';
+import { Text } from 'react-native';
+import Home from './app/pages/Home';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default function App({ navigation }) {
+    return (
+        <>
+            <Routes/>
+            <Text onPress={()=>{navigation.navigate(Home)}}>AAAAAAAAAAAAAA</Text>
+        </>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ app/pages/Home/index.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+const Home = () => {
+    <Text style={{marginTop: 40}}>Home</Text>
+}
+
+export default Home;
